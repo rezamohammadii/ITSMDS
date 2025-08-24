faed2476fb52e8c679536d4374ea45ac0fec0d96
MohammadReza Mohammadi <ps10mrm.rm@gmail.com>
2025-08-24 14:06:50 +0330

    not stable

--- ITSMDS.Infrastructure/Services/UserService.cs
+++ ITSMDS.Infrastructure/Services/UserService.cs
@@ -1,12 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 
-namespace ITSMDS.Infrastructure.Services
+using ITSMDS.Core.Application.Abstractions;
+using ITSMDS.Core.Application.DTOs;
+using ITSMDS.Core.Application.Services;
+
+namespace ITSMDS.Infrastructure.Services;
+
+public class UserService : IUserService
 {
-    internal class UserService
+    private readonly IUserRepository _repo;
+    private readonly IUnitOfWork _unitOfWork;
+    public UserService(IUnitOfWork unitOfWork, IUserRepository repo)
     {
+        _unitOfWork = unitOfWork;
+        _repo = repo;
+    }
+    public async Task<UserResponse> CreateAsync(CreateUserRequest request, CancellationToken ct = default)
+    {
+        if (await _re)
+        {
+            
+        }
+    }
+
+    public Task<List<UserResponse>> GetAllAsync(CancellationToken ct = default)
+    {
+        throw new NotImplementedException();
     }
 }
